e6ddcf2eea4b480696e3d74ea34f311704181328
StalinMazaEpn <stalinct97@gmail.com>
2019-09-08 10:46:26 -0500

    Aniadido Favicon

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/2ef3827640.js"></script>
     <link rel="stylesheet" href="./assets.css">
-
+	<link rel="icon" type="image/png" href="favicon.png">
     <title>Listado de Musicas</title>
     <script src="main.js"></script>
   </head>
